1b25df4e416553801a4ba360acd69e8f18e80011
Rahul Garg <rahulgarg@Rahuls-MacBook.local>
2014-07-07 22:58:52 +0530

    Initial Commit

---
+++ UITableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UITableView
+//
+//  Created by Rahul Garg on 07/07/14.
+//  Copyright (c) 2014 Rahul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableView
+//
+//  Created by Rahul Garg on 07/07/14.
+//  Copyright (c) 2014 Rahul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
